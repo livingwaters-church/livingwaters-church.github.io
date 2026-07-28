649833d0578d29649dd774b0d6cd3ab5f62ee59b
dndplus5 <152653893+dndplus5@users.noreply.github.com>
2026-07-27 21:10:19 -0500

    Update announcements in ticker.js
    Removed an announcement about the Ron Brown Summer Lunch Program and updated the Food Pantry announcement.

--- assets/JS/ticker.js
+++ assets/JS/ticker.js
@@ -4,10 +4,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!ticker) return;
 
     const announcements = [
-        '🚨 <strong>Ron Brown Summer Lunch Program:</strong> Was a great success, thank you to all the volunteers :)',
         '🎒 <strong>2026 Backpack Giveaway:</strong> Friday, August 7th at 11:30 AM in memory of Helen J. Stanfill!',
-        '📦 <strong>Food Pantry:</strong> Elisha\'s Closet distribution coming up this Friday @ 9:00am!',
-        '🍎 <strong>Feeding America:</strong> Regional mass food distribution hits the 2nd Tuesday of the month!',
+        '💦 <strong>Youth Ministry Event Scheduled:</strong> Water Balloon Fight, Aug 1st 2-4pm :)',      
+        '📦 <strong>Food Pantry:</strong> Elisha\'s Closet and Food distribution 3rd Friday @ 9:00am!',
+        '👔 <strong>Men\'s Ministry:</strong> Saturday, August 1st @ 09:00 AM!',
+        '🍎 <strong>Feeding America:</strong> Kentucky Based food distribution is the 2nd Tuesday of the month, Oak Grove Community Center!',
         '📱 <strong>RightNow Media:</strong> Free access to 25,000+ Bible study videos! Text LWKY to 49775 to join.'
     ];
 
