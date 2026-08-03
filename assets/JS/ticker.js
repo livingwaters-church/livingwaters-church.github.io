0d1c8458c2f5d6ec442f841dfe4e29b6a81b4a7d
dndplus5 <152653893+dndplus5@users.noreply.github.com>
2026-08-02 19:48:09 -0500

    Update announcements in ticker.js

--- assets/JS/ticker.js
+++ assets/JS/ticker.js
@@ -5,10 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const announcements = [
         '🎒 <strong>2026 Backpack Giveaway:</strong> Friday, August 7th at 11:30 AM in memory of Helen J. Stanfill!',
-        '💦 <strong>Youth Ministry Event Scheduled:</strong> Water Balloon Fight, Aug 1st 2-4pm :)',      
-        '📦 <strong>Food Pantry:</strong> Elisha\'s Closet and Food distribution 3rd Friday @ 9:00am!',
-        '👔 <strong>Men\'s Ministry:</strong> Saturday, August 1st @ 09:00 AM!',
-        '🍎 <strong>Feeding America:</strong> Kentucky Based food distribution is the 2nd Tuesday of the month, Oak Grove Community Center!',
+        '💦 <strong>Youth Ministry Event Scheduled:</strong> Womens Ministry, Aug 10 & 24 @ 5:30pm :)',
+        '🍎 <strong>Feeding America:</strong> Kentucky Based food distribution Tuesday 11 Aug 9-12pm, Oak Grove Community Center!',
+        '🎂 <strong>Birthday:</strong> 19 Aug, 1st Ladies Birthday!',
+        '📖 <strong>Awanas:</strong> Training Wednesday, August 19th & 26th @ 6pm!',
+        '📦 <strong>Food Pantry:</strong> Elisha\'s Closet and Food distribution Friday 21st @ 9:00am!',
+        '👔 <strong>Childrens Ministry  Qtr Meeting:</strong> Saturday, August 22 @ 10:00 AM!',
+        '🕊️ <strong>Baptism:</strong> Baptism Sunday!',
+        '👔 <strong>Singles Ministry:</strong> Saturday, August 29th @ 19:00 AM!',
+        '🎉 <strong>Pastors 6th Anniversary:</strong> Sunday Service',
+        '👔 <strong>Men\'s Ministry:</strong> Saturday, September 1st @ 09:00 AM!',
         '📱 <strong>RightNow Media:</strong> Free access to 25,000+ Bible study videos! Text LWKY to 49775 to join.'
     ];
 
