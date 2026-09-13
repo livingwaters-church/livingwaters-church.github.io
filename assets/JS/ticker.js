53787d2f13c6c92c5b55063efaebc401ddac1f8e
dndplus5 <152653893+dndplus5@users.noreply.github.com>
2026-09-13 18:23:51 -0500

    Update event announcements in ticker.js
    Updated announcements in ticker.js with new events and modified existing ones.

--- assets/JS/ticker.js
+++ assets/JS/ticker.js
@@ -4,17 +4,17 @@ document.addEventListener("DOMContentLoaded", () => {
     if (!ticker) return;
 
     const announcements = [
-        '🎒 <strong>2026 Backpack Giveaway:</strong> Friday, August 7th at 11:30 AM in memory of Helen J. Stanfill!',
-        '💦 <strong>Ministry Event Scheduled:</strong> Womens Ministry, Aug 10 & 24 @ 5:30pm :)',
-        '🍎 <strong>Feeding America:</strong> Kentucky Based food distribution Tuesday 11 Aug 9-12pm, Oak Grove Community Center!',
-        '🎂 <strong>Birthday:</strong> 19 Aug, 1st Ladies Birthday!',
-        '📖 <strong>Awanas:</strong> Training Wednesday, August 19th & 26th @ 6pm!',
-        '📦 <strong>Food Pantry:</strong> Elisha\'s Closet and Food distribution Friday 21st @ 9:00am!',
+        /*'🎒 <strong>2026 Backpack Giveaway:</strong> Friday, August 7th at 11:30 AM in memory of Helen J. Stanfill!',*/
+        '💦 <strong>Ministry Event Scheduled:</strong> Womens Ministry, Sep 14 & 28 @ 5:30pm :)',
+        '🍎 <strong>Feeding America:</strong> Kentucky Based food distribution Tuesday 15 9-12pm, Oak Grove Community Center!',
+        /*'🎂 <strong>Birthday:</strong> 19 Aug, 1st Ladies Birthday!',*/
+        '📖 <strong>Awanas:</strong> Wednesday\'s @ 6pm!',
+        '📦 <strong>Food Pantry:</strong> Elisha\'s Closet and Food Pantry Friday 25th @ 9:00am!',
         '👔 <strong>Childrens Ministry  Qtr Meeting:</strong> Saturday, August 22 @ 10:00 AM!',
-        '🕊️ <strong>Baptism:</strong> Baptism Sunday August 23rd!',
+        /*'🕊️ <strong>Baptism:</strong> Baptism Sunday August 23rd!',*/
         '👔 <strong>Singles Ministry:</strong> Saturday, August 29th @ 19:00 AM!',
-        '🎉 <strong>Pastors 6th Anniversary:</strong> Sunday Service',
-        '👔 <strong>Men\'s Ministry:</strong> Saturday, September 1st @ 09:00 AM!',
+        '🎉 <strong>Marriage Enrichment Night:</strong> Pinnacle Bowling Ally, Friday, 18th @ 6:00pm',
+        '👔 <strong>Men\'s Ministry:</strong> Saturday, September 12th @ 08:30 AM!',
         '📱 <strong>RightNow Media:</strong> Free access to 25,000+ Bible study videos! Text LWKY to 49775 to join.'
     ];
 
